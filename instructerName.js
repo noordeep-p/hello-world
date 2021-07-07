f18faf3b0b803b224fa9736e7a4d83005823b8e2
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-06 22:16:09 -0400

    Finished Kata 4 - Instructors Names

---
+++ instructerName.js
@@ -0,0 +1,28 @@
+const instructorWithLongestName = function(instructors) {
+  let nameLong = 0;
+  let longestName = {
+    name: '',
+    course: ''
+  }
+
+  for(const instructor of instructors){
+    if (instructor.name.length > nameLong){
+      nameLong = instructor.name.length;
+      longestName.name = instructor.name;
+      longestName.course = instructor.course;
+    }
+  } return longestName;
+};
+
+
+console.log(instructorWithLongestName([
+  {name: "Samuel", course: "iOS"},
+  {name: "Jeremiah", course: "Web"},
+  {name: "Ophilia", course: "Web"},
+  {name: "Donald", course: "Web"}
+]));
+console.log(instructorWithLongestName([
+  {name: "Matthew", course: "Web"},
+  {name: "David", course: "iOS"},
+  {name: "Domascus", course: "Web"}
+]));
