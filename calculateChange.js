b4d8740842bd251c053e3390bde44273d7ca45d7
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-14 17:23:21 -0400

    Finished kata 13 & 14

---
+++ calculateChange.js
@@ -0,0 +1,57 @@
+const calculateChange = function(total, cash){
+  changeBackObj = {};
+  let changeBack = 0;
+  changeBack = cash - total;
+  if(changeBack > 0){
+    if(changeBack > 2000){
+      change20 = changeBack/ 2000
+      changeBackObj.twentyDollar = Math.floor(change20);
+      changeBack = changeBack % 2000;
+    }
+    if(changeBack < 2000 && changeBack >= 1000){
+      change10 = changeBack/ 1000
+      changeBackObj.tenDollar = Math.floor(change10);
+      changeBack = changeBack % 1000;
+    }
+    if(changeBack < 100 && changeBack >= 500){
+      change5 = changeBack/ 500;
+      changeBackObj.fiveDollar = Math.floor(change5);
+      changeBack = changeBack % 500;
+    }
+    if(changeBack < 500 && changeBack >= 200){
+      change2 = changeBack/ 200;
+      changeBackObj.twoDollar = Math.floor(change2);
+      changeBack = changeBack % 200;
+    }
+    if(changeBack < 200 && changeBack >= 100){
+      change1 = changeBack/ 100;
+      changeBackObj.oneDollar = Math.floor(change1);
+      changeBack = changeBack % 100;
+    }
+    if(changeBack < 100 && changeBack >= 25){
+      changeQ = changeBack/ 25;
+      changeBackObj.quarter = Math.floor(changeQ);
+      changeBack = changeBack % 25;
+    }
+    if(changeBack < 25 && changeBack >= 10){
+      changeD = changeBack/ 10;
+      changeBackObj.dime = Math.floor(changeD);
+      changeBack = changeBack % 10;
+    }
+    if(changeBack < 10 && changeBack >= 5){
+      changeN = changeBack/ 5;
+      changeBackObj.nickel = Math.floor(changeN);
+      changeBack = changeBack % 5;
+    }
+    if(changeBack < 5 && changeBack > 0){
+      changeP = changeBack/ 1;
+      changeBackObj.penny = Math.floor(changeP);
+      changeBack = changeBack % 1;
+    }   
+  return changeBackObj;
+  }
+};
+
+console.log(calculateChange(1787, 2000));
+console.log(calculateChange(2623, 4000));
+console.log(calculateChange(501, 1000));
---
+++ talkingCalendar.js
@@ -0,0 +1,62 @@
+const talkingCalendar = function(date){
+  let year = "";
+  let month = "";
+  let day = "";
+  year = date.substring(0,4);
+  month = date.substring(5,7);
+  day = date.substring(8,10);
+  switch(month){
+    case "01":
+      month = "Janaury";
+      break;
+    case "02":
+      month = "Febraury";
+      break;
+    case "03":
+      month = "March";
+      break;
+    case "04":
+      month = "April";
+      break;
+    case "05":
+      month = "May";
+      break;
+    case "06":
+      month = "June";
+      break;
+    case "07":
+      month = "July";
+      break;
+    case "08":
+      month = "August";
+      break;
+    case "09":
+      month = "September";
+      break;
+    case "10":
+      month = "October";
+      break;
+     case "11":
+      month = "November";
+      break;
+    case "12":
+      month = "December";
+      break;
+  };
+  if(day[0] === "0" && day[1] === "1"){
+    day = "1st,";
+  }else if(day[0] === "0" && day[1] === "2"){
+    day = "2nd,";
+  }else if(day[0] === "0" && day[1] === "3"){
+    day = "3rd,";
+  }else if(day[0] === "0"){
+    day = day[1] + "th,";
+  }else if(day[0] > "0"){
+    day = day + "th,";
+  };
+  return month + " " + day + " " + year;
+};
+
+console.log(talkingCalendar("2017/12/02"));
+console.log(talkingCalendar("2007/11/11"));
+console.log(talkingCalendar("1987/08/24"));
