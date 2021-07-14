e3559cc8335850dc648344d62e9122e05545f6bc
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-13 22:20:18 -0400

    fixed linting

--- chooseRecipe.js
+++ chooseRecipe.js
@@ -1,15 +1,14 @@
-const chooseRecipe = function(bakeryA, bakeryB, recipes) {
+const chooseRecipe = function(bakeryA, bakeryB, recipes){
   let matchingRec1 = false; 
   let matchingRec2 = false;
-
   for(const recipe of recipes){
     for(let j = 0; j < recipe.ingredients.length; j++){
-      for(let k = 0; k <= bakeryA.length; k++){
+      for(let k = 0; k < bakeryA.length; k++){
         if(bakeryA[k] === recipe.ingredients[j]){
           matchingRec1 = true;
         }
       }
-      for(let m = 0; m <= bakeryB.length; m++){
+      for(let m = 0; m < bakeryB.length; m++){
         if(bakeryB[m] === recipe.ingredients[j]){
           matchingRec2 = true;
         }
