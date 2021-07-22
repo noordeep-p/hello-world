dd6d7c62aed02f0b9fcf709c60acd7b6bf220f22
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-22 00:30:45 -0400

    refactored loopy lighhouse 2 to return a statement and not just use console logs

--- loopyLighthouseTwo.js
+++ loopyLighthouseTwo.js
@@ -1,13 +1,18 @@
 const loopyLighthouse = function(range, multiples, words){
+  const outputArry = [];
   for (let i = range[0]; i <= range[1]; i++){
     if(i % multiples[0] === 0 && i % multiples[1] === 0){
-      console.log(words[0] + words[1]);
+      outputArry.push(words[0] + words[1] + '\n');
     } else if (i % multiples[0] === 0){
-     console.log(words[0]);
+      outputArry.push(words[0] + '\n');
     } else if (i % multiples[1] === 0){
-      console.log(words[1]);
-    } else console.log(i);  
+      outputArry.push(words[1] + '\n');
+    } else outputArry.push(i + '\n');
   }
+  finalOutput = outputArry[outputArry.length - 1].slice(0,-1);
+  outputArry.pop();
+  outputArry.push(finalOutput);
+  return console.log(outputArry.join(''));
 };
 
 loopyLighthouse([15, 90], [2, 5], ["Batty", "Beacon"]);
