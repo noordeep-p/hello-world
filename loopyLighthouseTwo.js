bf57ad2c12e915cf09f2b4a09d523bff2a26c40a
Noordeep Punjabi <punjabi.noordeep@gmail.com>
2021-07-22 00:40:21 -0400

    fixed linting errors

--- loopyLighthouseTwo.js
+++ loopyLighthouseTwo.js
@@ -1,15 +1,15 @@
-const loopyLighthouse = function(range, multiples, words){
+const loopyLighthouse = function(range, multiples, words) {
   const outputArry = [];
-  for (let i = range[0]; i <= range[1]; i++){
-    if(i % multiples[0] === 0 && i % multiples[1] === 0){
+  for (let i = range[0]; i <= range[1]; i++) {
+    if (i % multiples[0] === 0 && i % multiples[1] === 0) {
       outputArry.push(words[0] + words[1] + '\n');
-    } else if (i % multiples[0] === 0){
+    } else if (i % multiples[0] === 0) {
       outputArry.push(words[0] + '\n');
-    } else if (i % multiples[1] === 0){
+    } else if (i % multiples[1] === 0) {
       outputArry.push(words[1] + '\n');
     } else outputArry.push(i + '\n');
   }
-  finalOutput = outputArry[outputArry.length - 1].slice(0,-1);
+  let finalOutput = outputArry[outputArry.length - 1].slice(0,-1);
   outputArry.pop();
   outputArry.push(finalOutput);
   return console.log(outputArry.join(''));
